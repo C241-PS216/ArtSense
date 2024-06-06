68f8c9cbb4c3075c400d408c1200ecdc86f2f6f4
rishar69 <harisharyarevano@gmail.com>
2024-06-06 14:27:20 +0700

    Adding UI

--- app/src/main/java/com/harish/artsense/MainActivity.kt
+++ app/src/main/java/com/harish/artsense/MainActivity.kt
@@ -1,20 +1,71 @@
 package com.harish.artsense
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.harish.artsense.databinding.ActivityMainBinding
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val drawerLayout: DrawerLayout = binding.mainDrawer
+        val navView: NavigationView = binding.navView
+
+        val toolbar: Toolbar = binding.mainAppBar.toolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openNav, R.string.closeNav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+        toggle.drawerArrowDrawable.color = Color.WHITE
+
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.aboutMenu -> Toast.makeText(applicationContext, "aboutMenu", Toast.LENGTH_SHORT).show()
+                R.id.howItWorksMenu -> Toast.makeText(applicationContext, "how it works menu", Toast.LENGTH_SHORT).show()
+            }
+            drawerLayout.closeDrawers()
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (toggle.onOptionsItemSelected(item)) {
+            true
+        } else {
+            super.onOptionsItemSelected(item)
         }
     }
 }
